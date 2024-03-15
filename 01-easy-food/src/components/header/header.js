33e1806aa77ddfd7c1448143a7f7347ef845570b
MasterHellekin <clsjs94@hotmail.com>
2024-03-15 11:58:35 -0500

    header & navs

---
+++ 01-easy-food/src/components/header/header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import HeaderBackground from "@/components/header/header-background";
+
+import classes from "./header.module.css";
+
+import logoImg from "@/assets/logo.png";
+
+export default function Header() {
+  return (
+    <>
+      <HeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
