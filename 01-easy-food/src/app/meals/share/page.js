ed6721cf10b782d3ccd481a86d9cfb3cca7e7875
MasterHellekin <clsjs94@hotmail.com>
2024-03-20 17:01:26 -0500

    completed code

--- 01-easy-food/src/app/meals/share/page.js
+++ 01-easy-food/src/app/meals/share/page.js
@@ -1,3 +1,7 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
 
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
@@ -7,6 +11,7 @@ import { shareMeal } from "@/lib/actions";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +21,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -45,6 +50,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
